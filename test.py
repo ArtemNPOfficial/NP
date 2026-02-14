b59ae31d592a45f7cee7f1c3e3a169c713be5fee
tapik <133559915+ArtemNPOfficial@users.noreply.github.com>
2026-02-14 20:05:12 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-input()
+import telebot
+bot=telebot.TeleBot("8590892256:AAEtT89d9xy6NIMADjbBj9WvyDUN0jQubmI")
+
+@bot.message_handler(func=lambda msg: True)
+def response(msg):
+    bot.send_message(msg.chat.id, "Над ботом ведутся технические работы. Попробуйте позже.")
+
+bot.polling(none_stop=True)
